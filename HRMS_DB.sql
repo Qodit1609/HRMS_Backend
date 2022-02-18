9797644b12d13518f5914b372151bb047ca7b4ee
Abhishek Patel <abhishekpatel0201@gmail.com>
2022-02-18 19:02:08 +0530

    updated DB

--- HRMS_DB.sql
+++ HRMS_DB.sql
@@ -5,20 +5,21 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema TRH
+-- Schema HRMS_EDITED
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema TRH
+-- Schema HRMS_EDITED
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `TRH` DEFAULT CHARACTER SET utf8 ;
-USE `TRH` ;
+CREATE SCHEMA IF NOT EXISTS `HRMS_EDITED` DEFAULT CHARACTER SET utf8 ;
+USE `HRMS_EDITED` ;
 
 -- -----------------------------------------------------
--- Table `TRH`.`employees`
+-- Table `HRMS_EDITED`.`employees`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TRH`.`employees` (
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`employees` (
   `emp_id` INT NOT NULL AUTO_INCREMENT,
+  `role_type` VARCHAR(100) NOT NULL,
   `first_name` VARCHAR(100) NOT NULL,
   `last_name` VARCHAR(100) NOT NULL,
   `gender` VARCHAR(45) NOT NULL,
@@ -48,41 +49,43 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `TRH`.`job_title`
+-- Table `HRMS_EDITED`.`job_title`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TRH`.`job_title` (
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`job_title` (
   `job_id` INT NOT NULL AUTO_INCREMENT,
   `designation` VARCHAR(200) NOT NULL,
   `department` VARCHAR(200) NOT NULL,
   `job_type` VARCHAR(100) NOT NULL,
+  `Intern_mentor` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`job_id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `TRH`.`projects`
+-- Table `HRMS_EDITED`.`projects`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TRH`.`projects` (
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`projects` (
   `project_id` INT NOT NULL AUTO_INCREMENT,
   `job_id` INT NOT NULL,
   `project_name` VARCHAR(100) NOT NULL,
   `start_date` DATE NOT NULL,
   `end_date` DATE NOT NULL,
   `duration` DATETIME NOT NULL,
+  `reporting_manager` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`project_id`),
   INDEX `job_id_idx` (`job_id` ASC) VISIBLE,
   CONSTRAINT `job_id`
     FOREIGN KEY (`job_id`)
-    REFERENCES `TRH`.`job_title` (`job_id`)
+    REFERENCES `HRMS_EDITED`.`job_title` (`job_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `TRH`.`leave`
+-- Table `HRMS_EDITED`.`leave`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TRH`.`leave` (
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`leave` (
   `leave_id` INT NOT NULL AUTO_INCREMENT,
   `emp_id` INT NOT NULL,
   `start_date` DATE NOT NULL,
@@ -96,16 +99,16 @@ CREATE TABLE IF NOT EXISTS `TRH`.`leave` (
   INDEX `emp_id_idx` (`emp_id` ASC) VISIBLE,
   CONSTRAINT `emp_id`
     FOREIGN KEY (`emp_id`)
-    REFERENCES `TRH`.`employees` (`emp_id`)
+    REFERENCES `HRMS_EDITED`.`employees` (`emp_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `TRH`.`attendance`
+-- Table `HRMS_EDITED`.`attendance`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `TRH`.`attendance` (
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`attendance` (
   `atten_id` INT NOT NULL AUTO_INCREMENT,
   `project_id` INT NOT NULL,
   `leave_id` INT NOT NULL,
@@ -118,17 +121,42 @@ CREATE TABLE IF NOT EXISTS `TRH`.`attendance` (
   `login_duration` DATETIME NOT NULL,
   `lat_l` FLOAT NOT NULL,
   `long_l` FLOAT NOT NULL,
+  `break_time` DATETIME NOT NULL,
+  `break_counts` INT NOT NULL,
+  `break_reason` VARCHAR(500) NOT NULL,
   PRIMARY KEY (`atten_id`),
   INDEX `project_id_idx` (`project_id` ASC) VISIBLE,
   INDEX `leave_id_idx` (`leave_id` ASC) VISIBLE,
   CONSTRAINT `project_id`
     FOREIGN KEY (`project_id`)
-    REFERENCES `TRH`.`projects` (`project_id`)
+    REFERENCES `HRMS_EDITED`.`projects` (`project_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT `leave_id`
     FOREIGN KEY (`leave_id`)
-    REFERENCES `TRH`.`leave` (`leave_id`)
+    REFERENCES `HRMS_EDITED`.`leave` (`leave_id`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `HRMS_EDITED`.`atten_regularization`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `HRMS_EDITED`.`atten_regularization` (
+  `atten_reg_id` INT NOT NULL AUTO_INCREMENT,
+  `atten_id` INT NOT NULL,
+  `correction_date` DATE NOT NULL,
+  `Previous_time` DATETIME NOT NULL,
+  `updated_time` DATETIME NOT NULL,
+  `update_reason` VARCHAR(500) NOT NULL,
+  `update_status` TINYINT NOT NULL,
+  PRIMARY KEY (`atten_reg_id`),
+  UNIQUE INDEX `atten_reg_id_UNIQUE` (`atten_reg_id` ASC) VISIBLE,
+  INDEX `atten_id_idx` (`atten_id` ASC) VISIBLE,
+  CONSTRAINT `atten_id`
+    FOREIGN KEY (`atten_id`)
+    REFERENCES `HRMS_EDITED`.`attendance` (`atten_id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
